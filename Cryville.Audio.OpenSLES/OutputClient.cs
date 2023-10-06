75d4d8b105f61783d459eb3a8f93bb18ea2b7943
PopSlime <popslime@qq.com>
2023-10-06 13:49:12 +0800

    Revert "Adapt OpenSL output client to polling buffers to avoid GC in certain runtimes." (c427778b)

--- Cryville.Audio.OpenSLES/OutputClient.cs
+++ Cryville.Audio.OpenSLES/OutputClient.cs
@@ -1,7 +1,7 @@
+using Cryville.Common.Interop;
 using OpenSLES.Native;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Threading;
 
@@ -10,14 +10,19 @@ namespace Cryville.Audio.OpenSLES {
 	/// An <see cref="AudioClient" /> that interacts with OpenSL ES.
 	/// </summary>
 	public class OutputClient : AudioClient {
+		static readonly List<OutputClient> _instances = new List<OutputClient>();
+		readonly int _id;
+
 		internal OutputClient(Engine engine, OutputDevice device, WaveFormat format, float bufferDuration, AudioShareMode shareMode) {
+			_id = _instances.Count;
+			_instances.Add(this);
+
 			_objEngine = engine;
 			m_device = device;
 
 			if (shareMode == AudioShareMode.Exclusive)
 				throw new NotSupportedException("Exclusive mode not supported.");
 			if (bufferDuration == 0) bufferDuration = device.DefaultBufferDuration;
-			_bufferDuration = (int)bufferDuration;
 			m_format = format;
 
 			Guid[] ids = new Guid[4];
@@ -52,6 +57,8 @@ namespace Cryville.Audio.OpenSLES {
 				_buf[i] = new byte[m_bufferSize];
 				_hbuf[i] = GCHandle.Alloc(_buf[i], GCHandleType.Pinned);
 			}
+
+			Util.SLR(_bq.Obj.RegisterCallback(_bq, Callback, new IntPtr(_id)));
 		}
 
 		/// <inheritdoc />
@@ -68,6 +75,7 @@ namespace Cryville.Audio.OpenSLES {
 				_objMix?.Obj.Destroy(_objMix);
 				foreach (var h in _hbuf) h.Free();
 				foreach (var h in _handles) h.Free();
+				_instances.Remove(this);
 			}
 		}
 
@@ -87,7 +95,6 @@ namespace Cryville.Audio.OpenSLES {
 		/// <inheritdoc />
 		public override WaveFormat Format => m_format;
 
-		readonly int _bufferDuration;
 		readonly int m_bufferSize;
 		/// <inheritdoc />
 		public override int BufferSize => m_bufferSize;
@@ -113,69 +120,44 @@ namespace Cryville.Audio.OpenSLES {
 		readonly GCHandle[] _hbuf = new GCHandle[BUFFER_COUNT];
 
 		/// <inheritdoc />
-		public override void Start() {
-			if (!Playing) {
-				Util.SLR(_bq.Obj.GetState(_bq, out var state));
-				for (int i = (int)state.count; i < BUFFER_COUNT; i++) Enqueue();
-				_thread = new Thread(new ThreadStart(ThreadLogic)) {
-					Priority = ThreadPriority.Highest,
-					IsBackground = true,
-				};
-				_thread.Start();
-				Util.SLR(_play.Obj.SetPlayState(_play, (UInt32)SL_PLAYSTATE.PLAYING));
-				base.Start();
-			}
-		}
-
-		/// <inheritdoc />
 		public override void Pause() {
 			if (Playing) {
 				lock (_enqlock) {
-					_threadAbortFlag = true;
-					if (!_thread.Join(1000))
-						throw new InvalidOperationException("Failed to pause output client.");
-					_thread = null;
 					Util.SLR(_play.Obj.SetPlayState(_play, (UInt32)SL_PLAYSTATE.PAUSED));
 					base.Pause();
 				}
 			}
 		}
 
-		Thread _thread;
-		bool _threadAbortFlag;
-		readonly object _enqlock = new object();
-		void ThreadLogic() {
-			_threadAbortFlag = false;
-			Stopwatch timer = new Stopwatch();
-			while (true) {
-				int pendingBufferCount;
-				while (true) {
-					Util.SLR(_bq.Obj.GetState(_bq, out var state));
-					if (state.count < BUFFER_COUNT) {
-						pendingBufferCount = (int)state.count;
-						break;
-					}
-					Thread.Sleep(1);
-					if (_threadAbortFlag) return;
-				}
-				int timeout = _bufferDuration;
-				timer.Reset();
-				timer.Start();
-				for (int i = pendingBufferCount; i < BUFFER_COUNT; i++) Enqueue();
-				timeout -= (int)timer.ElapsedMilliseconds + 1;
-				if (timeout < 0) timeout = 0;
-				Thread.Sleep(timeout);
-				if (_threadAbortFlag) return;
+		/// <inheritdoc />
+		public override void Start() {
+			if (!Playing) {
+				Util.SLR(_bq.Obj.GetState(_bq, out var state));
+				for (int i = 0; i < BUFFER_COUNT - state.count; i++) Enqueue();
+				Util.SLR(_play.Obj.SetPlayState(_play, (UInt32)SL_PLAYSTATE.PLAYING));
+				base.Start();
 			}
 		}
+
+		readonly object _enqlock = new object();
+		int _bufc;
 		void Enqueue() {
 			lock (_enqlock) {
-				if (Source == null || Muted) Array.Clear(_buf[_bufi], 0, BufferSize);
+				if (_bufc >= BUFFER_COUNT) return;
+				_bufc++;
+				if (Muted) Array.Clear(_buf[_bufi], 0, BufferSize);
 				else Source.Read(_buf[_bufi], 0, BufferSize);
 				Util.SLR(_bq.Obj.Enqueue(_bq, _hbuf[_bufi++].AddrOfPinnedObject(), (uint)BufferSize));
 				_bufi %= BUFFER_COUNT;
 				m_bufferPosition += (double)BufferSize / Format.BytesPerSecond;
 			}
+		}
+
+		[MonoPInvokeCallback]
+		static void Callback(IntPtr caller, IntPtr pContext) {
+			var i = _instances[pContext.ToInt32()];
+			i._bufc--;
+			i.Enqueue();
 		}
 	}
 }
