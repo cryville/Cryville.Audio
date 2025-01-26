56f4077f80944e72b77c1fb7b088807540ff1ccf
PopSlime <popslime@qq.com>
2025-01-26 13:13:53 +0800

    fix(core)!: Make EngineBuilder non-static

--- Cryville.Audio/EngineBuilder.cs
+++ Cryville.Audio/EngineBuilder.cs
@@ -6,11 +6,11 @@ namespace Cryville.Audio {
 	/// <summary>
 	/// The recommended entry for Cryville.Audio that creates an <see cref="IAudioDeviceManager" />.
 	/// </summary>
-	public static class EngineBuilder {
+	public class EngineBuilder {
 		/// <summary>
 		/// The list of available engines.
 		/// </summary>
-		public static readonly IList<Type> Engines = [];
+		public IList<Type> Engines { get; } = [];
 
 		/// <summary>
 		/// Creates a <see cref="IAudioDeviceManager" /> in the <see cref="Engines" /> list.
@@ -20,7 +20,7 @@ namespace Cryville.Audio {
 		/// <para>Add engines to <see cref="Engines" /> before calling this method.</para>
 		/// </remarks>
 		[SuppressMessage("Design", "CA1031")]
-		public static IAudioDeviceManager? Create() {
+		public IAudioDeviceManager? Create() {
 			foreach (var type in Engines) {
 				if (!typeof(IAudioDeviceManager).IsAssignableFrom(type)) continue;
 				try {
